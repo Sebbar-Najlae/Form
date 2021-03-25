f7adee977f04568d7198eae95ffff2b73c86f346
sebbar najlae <sebbar.najlae.dev@gmail.com>
2021-03-25 15:51:32 +0100

    formulaire

---
+++ Inscription/inscription.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+    <link rel="stylesheet" href="inscription.css">
+
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$nomErr = $emailErr = $prenomErr =   "";
+$nom = $email   = $prenom = $ville = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["nom"])) {
+    $nomErr = "ce champ est obligatoire";
+  } else {
+    $nom = test_input($_POST["nom"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$nom)) {
+      $nomErr = "Seul les lettres et les espaces sont validés";
+    }
+  }
+
+  if (empty($_POST["prenom"])) {
+    $prenomErr = "ce champ est obligatoire";
+  } else {
+    $prenom = test_input($_POST["prenom"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$prenom)) {
+      $prenomErr = "Seul les lettres et les espaces sont validés";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "ce champ est obligatoire";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Format invalide";
+    }
+  }
+    
+ }
+
+
+ 
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+
+
+<form name="fo" method="post" action="">
+         <fieldset>
+            <legend>Nouvel utilisateur</legend>
+            
+          
+            <div class="label">Nom</div>
+            <div class="champ">
+               <input type="text" name="nom" value="<?php echo $nom?>" />
+               <span class="error">* <?php echo $nomErr;?></span>
+            </div>
+
+            <div class="label">Prénom</div>
+            <div class="champ">
+               <input type="text" name="prenom" value="<?php echo $prenom?>" />
+               <span class="error">* <?php echo $prenomErr;?></span>
+            </div>
+            <div class="label">Email</div>
+            <div class="champ">
+               <input type="text" name="email" value="<?php echo $email?>" />
+               <span class="error">* <?php echo $emailErr;?></span>
+            </div>
+            <div class="label">Ville</div>
+            <div class="champ">
+               <select name="ville">
+                  <option <?php if($ville=="Tanger") echo "selected";?>>Tanger</option>
+                  <option <?php if($ville=="Tetouan") echo "selected";?>>Tetouan</option>
+                  <option <?php if($ville=="Laarache") echo "selected";?>>Laarache</option>
+               </select>
+            </div>
+            <div class="champ">
+               <input type="submit" name="valider" value="Valider l'inscription" />
+            </div>
+         </fieldset>
+      </form>
+
+      <?php
+echo "<h2>Your Input:</h2>";
+echo $nom;
+echo "<br>";
+echo $prenom;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $ville;
+echo "<br>";
+
+?>
+
+</body>
+</html>
