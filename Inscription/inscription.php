ce2c7afe8ca7e2b8b06b8a6731e7084447832cc6
sebbar najlae <sebbar.najlae.dev@gmail.com>
2021-03-26 09:25:37 +0100

    matin

--- Inscription/inscription.php
+++ Inscription/inscription.php
@@ -99,12 +99,12 @@ function test_input($data) {
       <?php
 
 
-$nom=$_POST['nom'];
-$prenom=$_POST['prenom'];
-$email=$_POST['email'];
-$ville=$_POST['ville'];
 
-$tabs = array($nom,$prenom,$email,$ville);
+
+
+
+
+$tabs = array($_POST['nom'],$_POST['prenom'],$_POST['email'],$_POST['ville']);
 foreach ($tabs as $tab) {
     echo " $tab \n" ;
 }
